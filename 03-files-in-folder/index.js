426ce6ed722438c66bb1408896177ba8086cc806
Vlad <makrak.vladislav@yandex.ru>
2022-05-19 21:02:49 +0300

    feat: add 03-files-in-folder task resolve

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const { readdir } = require('fs/promises');
+const fs = require('fs');
+const path  = require('path');
+const filePath = path.join(__dirname, '', 'secret-folder');
+(async function (_path) {
+  const files = await readdir(_path);
+  files.forEach(file => {
+    const ext = path.extname(file).replace(/\./g, '');
+    const name = file.split('.')[0];
+    fs.stat(filePath + '\\' + file, (err, stats) => {
+      const fileSize = stats.size;
+      if (stats.isDirectory() === false) {
+        console.log(`${name} - ${ext} - ${fileSize}byt`);
+      }
+    });
+  });
+})(filePath);
