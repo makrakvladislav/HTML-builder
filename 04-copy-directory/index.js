2918fbe111a5798720d6627e5c931d36b8531229
Vlad <makrak.vladislav@yandex.ru>
2022-05-23 19:50:42 +0300

    fix: fix path and async function

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,15 +2,17 @@ const fs = require('fs');
 const fsPromises = fs.promises;
 const { readdir, copyFile } = require('fs/promises');
 const path  = require('path');
-const folderPath = path.join(__dirname, '', 'files');
+const folderPath = path.join(__dirname, 'files');
+const folderCopy = path.join(__dirname, 'files-copy');
 
-fsPromises.mkdir(`${__dirname}\\files-copy`, { recursive: true }).then(function() {
+const copyDirectory = () => {
+  fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
   (async function (_path) {
     const files = await readdir(_path);
     files.forEach(file => {
       fsPromises.unlink(path.join(__dirname, 'files-copy', file));
     });
-  })(`${__dirname}\\files-copy`);
+  })(folderCopy);
 
   (async function (_path) {
     const files = await readdir(_path, {withFileTypes: true});
@@ -20,4 +22,12 @@ fsPromises.mkdir(`${__dirname}\\files-copy`, { recursive: true }).then(function(
       }
     });
   })(folderPath);
-});
+};
+
+const copy = async function() {
+  copyDirectory();
+};
+
+copy();
+  
+
